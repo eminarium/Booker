2b133ebd4a95acd569c0cd15b248f4c6e03744ae
Merdan Durdiyev <merdan.durdiyev@gmail.com>
2019-05-15 14:39:06 +0500

    Added user references to Books, Authors and Reviews

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_15_070006) do
+ActiveRecord::Schema.define(version: 2019_05_15_093504) do
 
   create_table "authors", force: :cascade do |t|
     t.string "first_name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2019_05_15_070006) do
     t.integer "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "books", force: :cascade do |t|
@@ -28,6 +29,7 @@ ActiveRecord::Schema.define(version: 2019_05_15_070006) do
     t.integer "author_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
     t.index ["author_id"], name: "index_books_on_author_id"
   end
 
@@ -36,7 +38,9 @@ ActiveRecord::Schema.define(version: 2019_05_15_070006) do
     t.integer "book_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
     t.index ["book_id"], name: "index_reviews_on_book_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
