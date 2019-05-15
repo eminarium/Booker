ceb4047f689ebb37dedd0721002974c735fb7164
Merdan Durdiyev <merdan.durdiyev@gmail.com>
2019-05-15 12:00:43 +0500

    Installed and set up Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :reviews
   resources :books
   resources :authors
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "books#index"
 end
