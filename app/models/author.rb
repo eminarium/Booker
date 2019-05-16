a05b350ea2384d0c4f9bb780609dac3fed31394b
Merdan Durdiyev <merdan.durdiyev@gmail.com>
2019-05-16 19:02:05 +0500

    Decorated books and authors index and show pages

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,6 @@
 class Author < ApplicationRecord
     belongs_to :user
+    has_many :authors, dependent: :destroy
 
     def first_and_last_name
         self.first_name[0]+". "+self.last_name
--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,8 @@ class Book < ApplicationRecord
   belongs_to :author
   belongs_to :user
 
+  has_many :reviews, dependent: :destroy
+
   has_attached_file :image #, styles: { medium: ["400x600>", :jpg] }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/  
 end
